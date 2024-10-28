017bc55fc3ebd9c5db4897cc50d46766b46764fb
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-10-28 16:26:24 +0000

    Add tango option with fastcs proposal API

--- src/thorlabs_mff_fastcs/__main__.py
+++ src/thorlabs_mff_fastcs/__main__.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 import typer
-from fastcs.connections.serial_connection import SerialConnectionSettings
+from fastcs import FastCS
+from fastcs.connections import SerialConnectionSettings
 
 from thorlabs_mff_fastcs.controllers import (
     ThorlabsMFF,
@@ -22,6 +23,13 @@ def version_callback(value: bool):
         raise typer.Exit()
 
 
+def get_controller(port: str, baud: int) -> ThorlabsMFF:
+    serial_settings = SerialConnectionSettings(port=port, baud=baud)
+    settings = ThorlabsMFFSettings(serial_settings)
+    cont = ThorlabsMFF(settings)
+    return cont
+
+
 @app.callback()
 def main(
     version: bool | None = typer.Option(
@@ -37,7 +45,7 @@ def main(
 
 @app.command()
 def ioc(
-    pv_prefix: str = typer.Argument(..., help="Name of the IOC/service to attach to"),
+    pv_prefix: str = typer.Argument(..., help="IOC PV prefix"),
     port: str = typer.Argument(..., help="Serial port to open such as /dev/ttyUSB0"),
     baud: int = typer.Option(115200, help="Baud rate"),
     output_path: Path = typer.Option(  # noqa: B008
@@ -54,18 +62,47 @@ def ioc(
     """
     Start up the service
     """
-    from fastcs.backends.epics.backend import EpicsBackend, EpicsGUIOptions
+    from fastcs.backends import EpicsGUIOptions, EpicsIOCOptions, EpicsOptions
 
-    backend = EpicsBackend(get_controller(port, baud), pv_prefix)
-    backend.create_gui(EpicsGUIOptions(output_path / "index.bob"))
-    backend.run()
+    options = EpicsOptions(
+        ioc=EpicsIOCOptions(pv_prefix=pv_prefix),
+        gui=EpicsGUIOptions(output_path / "index.bob"),
+    )
+
+    fast_cs = FastCS(get_controller(port, baud), options)
+    fast_cs.create_gui()
+    fast_cs.run()
 
 
-def get_controller(port: str, baud: int) -> ThorlabsMFF:
-    serial_settings = SerialConnectionSettings(port=port, baud=baud)
-    settings = ThorlabsMFFSettings(serial_settings)
-    tcont = ThorlabsMFF(settings)
-    return tcont
+@app.command()
+def dsr(
+    dev_name: str = typer.Argument(
+        ..., help="Name of the device instance .e.g my/device/name"
+    ),
+    dsr_name: str = typer.Argument(
+        ..., help="Name of the device server instance e.g. my_server-instance"
+    ),
+    port: str = typer.Argument(..., help="Serial port to open such as /dev/ttyUSB0"),
+    baud: int = typer.Option(115200, help="Baud rate"),
+):
+    """
+    Start up the service
+    """
+
+    from fastcs.backends import TangoDSROptions, TangoOptions
+
+    controller = get_controller(port, baud)
+    device_options = TangoOptions(
+        TangoDSROptions(
+            dev_class=controller.__class__.__name__,
+            debug=False,
+            dev_name=dev_name,
+            dsr_instance=dsr_name,
+        )
+    )
+
+    fast_cs = FastCS(controller, device_options)
+    fast_cs.run()
 
 
 # test with: python -m thorlabs_mff_fastcs
