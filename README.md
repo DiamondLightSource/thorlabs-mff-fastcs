20d2bc3334d9975a47f6e55e2a7a14ca8811a000
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2025-01-27 11:17:08 +0000

    Update copeir template

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 [![CI](https://github.com/DiamondLightSource/thorlabs-mff-fastcs/actions/workflows/ci.yml/badge.svg)](https://github.com/DiamondLightSource/thorlabs-mff-fastcs/actions/workflows/ci.yml)
 [![Coverage](https://codecov.io/gh/DiamondLightSource/thorlabs-mff-fastcs/branch/main/graph/badge.svg)](https://codecov.io/gh/DiamondLightSource/thorlabs-mff-fastcs)
 [![PyPI](https://img.shields.io/pypi/v/thorlabs-mff-fastcs.svg)](https://pypi.org/project/thorlabs-mff-fastcs)
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)
 
 # thorlabs_mff_fastcs
 
-FastCS support for the [ThorLabs MFF](https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=3962) (Motorized Filter Flipper)
+FastCS support for the ThorLabs MFF (Motorized Filter Flipper)
 
 This is where you should write a short paragraph that describes what your module does,
 how it does it, and why people should use it.
--- src/thorlabs_mff_fastcs/__main__.py
+++ src/thorlabs_mff_fastcs/__main__.py
@@ -1,73 +1,24 @@
-from pathlib import Path
+"""Interface for ``python -m thorlabs_mff_fastcs``."""
 
-import typer
-from fastcs.connections.serial_connection import SerialConnectionSettings
-
-from thorlabs_mff_fastcs.controllers import (
-    ThorlabsMFF,
-    ThorlabsMFFSettings,
-)
+from argparse import ArgumentParser
+from collections.abc import Sequence
 
 from . import __version__
 
 __all__ = ["main"]
 
 
-app = typer.Typer()
-
-
-def version_callback(value: bool):
-    if value:
-        typer.echo(__version__)
-        raise typer.Exit()
-
-
-@app.callback()
-def main(
-    version: bool | None = typer.Option(
-        None,
+def main(args: Sequence[str] | None = None) -> None:
+    """Argument parser for the CLI."""
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-v",
         "--version",
-        callback=version_callback,
-        is_eager=True,
-        help="Print the version and exit",
-    ),
-):
-    pass
+        action="version",
+        version=__version__,
+    )
+    parser.parse_args(args)
 
 
-@app.command()
-def ioc(
-    pv_prefix: str = typer.Argument(..., help="Name of the IOC/service to attach to"),
-    port: str = typer.Argument(..., help="Serial port to open such as /dev/ttyUSB0"),
-    baud: int = typer.Option(115200, help="Baud rate"),
-    output_path: Path = typer.Option(  # noqa: B008
-        Path.cwd(),  # noqa: B008
-        help="folder of local service definition",
-        exists=True,
-        file_okay=False,
-        dir_okay=True,
-        writable=False,
-        readable=True,
-        resolve_path=True,
-    ),
-):
-    """
-    Start up the service
-    """
-    from fastcs.backends.epics.backend import EpicsBackend, EpicsGUIOptions
-
-    backend = EpicsBackend(get_controller(port, baud), pv_prefix)
-    backend.create_gui(EpicsGUIOptions(output_path / "index.bob"))
-    backend.run()
-
-
-def get_controller(port: str, baud: int) -> ThorlabsMFF:
-    serial_settings = SerialConnectionSettings(port=port, baud=baud)
-    settings = ThorlabsMFFSettings(serial_settings)
-    tcont = ThorlabsMFF(settings)
-    return tcont
-
-
-# test with: python -m thorlabs_mff_fastcs
 if __name__ == "__main__":
-    app()
+    main()
